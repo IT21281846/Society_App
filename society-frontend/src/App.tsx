21a014a8182643fd45ad8523ba8358c53717a44f
Tharusha Hapuarachchi <tharuhapuarachchi00@gmail.com>
2025-09-26 17:16:10 +0530

    frontend created

--- society-frontend/src/App.tsx
+++ society-frontend/src/App.tsx
@@ -33,3 +33,10 @@ function App() {
 }
 
 export default App
+
+
+{/* <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working! 🎉
+      </h1>
+    </div> */}
